6c59a9d498235c284f0c01ebeeef44edf386d5bd
Stephen Lauck <lauck@chef.io>
2016-08-23 16:52:22 -0400

    add patch update download and install w/ reboot

--- recipes/windows_patch.rb
+++ recipes/windows_patch.rb
@@ -1,1 +1,13 @@
-include_recipe 'wsus-client'
+return unless platform?('windows')
+
+include_recipe 'wsus-client::configure'
+
+reboot 'Restart Computer' do
+  action :nothing
+  only_if {reboot_pending?}
+end
+
+wsus_client_update 'WSUS updates' do
+  action [:download, :install]
+  notifies :reboot_now, 'reboot[Restart Computer]', :immediately
+end
